0b1492f54a77a0f9278de93dfb31b93d2dd04fb2
thminh176 <thminh.176.vn@gmail.com>
2024-10-11 00:14:21 +0700

    Cập nhật file cấu hình và data

--- api/server.js
+++ api/server.js
@@ -1,8 +1,7 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Đảm bảo đường dẫn này đúng
+const router = jsonServer.router("../db.json");
 const middlewares = jsonServer.defaults();
-
 server.use(middlewares);
 server.use(router);
 

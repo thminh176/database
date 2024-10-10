e67b5e2b5ad86958f40a77d2f9b3af5f5c31854d
thminh176 <thminh.176.vn@gmail.com>
2024-10-11 00:24:45 +0700

    Cập nhật cấu hình server.js và vercel.json

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); // Đảm bảo rằng 'db.json' nằm ở đúng vị trí
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);

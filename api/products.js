2d30967966d35b3e4d802795e33767fc9f74faa1
thminh176 <thminh.176.vn@gmail.com>
2024-10-11 00:41:47 +0700

    lon

--- api/products.js
+++ api/products.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = (req, res) => {
   try {
-    const data = fs.readFileSync('./db.json', 'utf8');
+    const data = fs.readFileSync("./db.json", "utf8");
     const products = JSON.parse(data).products;
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
